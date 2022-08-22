3957bb7ffbc3fb4e9b286e2d96cb4dff03f299a8
Dhyey Moliya <dhyeymoliya@gmail.com>
2022-08-22 16:14:45 +0530

    Added Task Card Style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,93 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { DateTime } from 'luxon';
+	let adding = false;
+</script>
+
+<h1 class="text-center mb-4">To Do Manager</h1>
+<section class="row justify-content-center mb-3">
+	<div class="col-8">
+		<div class="card shadow shadow-sm mb-3 ">
+			<div class="card-body">
+				<div class="row">
+					<div class="col">
+						<h4 class="card-title">Task 1</h4>
+						<div class="card-text text-muted mb-2">Task 1 Description</div>
+						<div class="card-text text-muted timestamp">
+							{DateTime.fromJSDate(new Date()).toFormat('dd MMM, yyyy hh:mm a')}
+						</div>
+					</div>
+					<div class="col-auto">
+						<button class="btn btn-sm btn-outline-primary"> Mark Complete </button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="card mb-3 bg-light border-0">
+			<div class="card-body">
+				<div class="row">
+					<div class="col">
+						<h4 class="card-title text-decoration-line-through">Task 2</h4>
+						<div class="card-text text-muted text-decoration-line-through mb-2">
+							Task 2 Description
+						</div>
+						<div class="card-text text-muted timestamp">
+							{DateTime.fromJSDate(new Date()).toFormat('dd MMM, yyyy hh:mm a')}
+						</div>
+					</div>
+					<div class="col-auto">
+						<button class="btn btn-sm btn-outline-secondary"> Mark Active </button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="row justify-content-center mb-3">
+	{#if !adding}
+		<div class="col-auto">
+			<button
+				class="btn btn-outline-primary"
+				type="button"
+				on:click={() => {
+					adding = true;
+				}}>New Task</button
+			>
+		</div>
+	{:else}
+		<div class="col-6">
+			<form class="border shadow-sm shadow-primary rounded p-3 bg-light">
+				<h3>Create New Task</h3>
+				<div class="mb-3">
+					<label for="new-task-title" class="form-label">Task Title</label>
+					<input type="text" class="form-control" id="new-task-title" />
+				</div>
+				<div class="mb-3">
+					<label for="new-task-description" class="form-label">Task Description</label>
+					<textarea rows="4" class="form-control" id="new-task-description" />
+				</div>
+				<div class="text-center">
+					<button
+						class="btn btn-primary"
+						type="button"
+						on:click={() => {
+							adding = false;
+						}}>Create To Do</button
+					>
+					<button
+						class="btn btn-outline-secondary"
+						type="button"
+						on:click={() => {
+							adding = false;
+						}}>Cancel</button
+					>
+				</div>
+			</form>
+		</div>
+	{/if}
+</section>
+
+<style>
+	.timestamp {
+		font-size: 0.75rem;
+	}
+</style>
