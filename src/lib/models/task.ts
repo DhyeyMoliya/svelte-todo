fe0d1380fde939a635beeb13137015401eea5d71
Dhyey Moliya <dhyeymoliya@gmail.com>
2022-08-22 16:42:21 +0530

    Updated Task Model

--- src/lib/models/task.ts
+++ src/lib/models/task.ts
@@ -1,33 +1,43 @@
 import { Schema, model, models } from 'mongoose';
 
-export interface ITask extends DeepPartial<{
-    title: string,
-    description: string,
-    createdAt: Date
-    updatedAt: Date
-}> { }
+export interface ITask
+	extends DeepPartial<{
+		title: string;
+		description: string;
+		dueDate: Date;
+		completed: boolean;
+		completedAt: Date;
+		createdAt: Date;
+		updatedAt: Date;
+	}> {}
 
-const taskSchema = new Schema<ITask>({
-    title: String,
-    description: String,
-}, {
-    timestamps: true,
-    toJSON: {
-        virtuals: true,
-    },
-    toObject: {
-        virtuals: true,
-    },
-    collation: {
-        locale: 'en_US',
-        numericOrdering: true,
-    },
-});
+const taskSchema = new Schema<ITask>(
+	{
+		title: String,
+		description: String,
+		dueDate: Date,
+		completed: { type: Boolean, default: false },
+		completedAt: Date
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			virtuals: true
+		},
+		toObject: {
+			virtuals: true
+		},
+		collation: {
+			locale: 'en_US',
+			numericOrdering: true
+		}
+	}
+);
 
 if (models['Task']) {
-    delete models['Task'];
+	delete models['Task'];
 }
 
 export const Task = model<ITask>('Task', taskSchema);
 
-Task.syncIndexes().catch((err) => { });
+Task.syncIndexes().catch((err) => {});
